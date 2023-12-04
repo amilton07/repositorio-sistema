b2e6f5aaf003c350c9da64473771df367e4ee13b
amilton07 <129789503+amilton07@users.noreply.github.com>
2023-12-04 14:17:48 -0300

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,60 +1,44 @@
 const express = require("express")
-const {z, ZodError} = require("zod")
+const {z, Schema, ZodError} = require("zod")
+const {PrismaClient} = require("@prisma/client")
+
+const prisma = new PrismaClient();
+
 const app = express()
 
 app.use(express.json())
 
-app.get("/professores", function(req,res){
-    res.status(200).json([])
+app.get("/professores", async function (req, res) {
+    const professores =  await prisma.professor.findMany()
+    res.status(200).json(professores)
 })
 
-
-app.post("/professores", function(req, res) {
-    try{
-        const{nome, email , matricula }= req.body
-        console.log("nome", nome)
-        console.log("email", email )
-        console.log("matricula", matricula)
-        res.status(200).send()
-    }catch(err){
-
-    }
-  
-})
- 
- app.post("/professores",function(req,res){
-    try{
-        const schema= z.object({
+app.post("/professores", function (req, res) {
+    try {
+        const createProfessorSchema = z.object({
             nome: z.string(),
             email: z.string().email(),
             matricula: z.string()
-
-        })   
-        const{nome,email,matricula}=schema.parse(req.body)
-            
-        console.log("nome",nome)
-        console.log("email",email)
-        console.log("matricula",matricula)
-
-        res.status(200)
+        })
+        const {nome, email, matricula} = createProfessorSchema.parse(req.body)
+        console.log("nome", nome)
+        console.log("email", email)
+        console.log("matricula", matricula)
+        res.status(200).send("Deu Certo")
     }catch(err){
         if(err instanceof ZodError){
             res.status(422).send(err.message)
             return
         }
-        res.status(500).send(err.message)
+        res.status(500). send(err.message)
+    }finally {
+
     }
-            
-
 })
 
-
- 
- app.listen(333, function(){
-
-    console.log(`vai viu, pai e gueto `)
+app.listen(3333, function () {
+    console.log(`Http running server`);
 })
-
 
 
 
